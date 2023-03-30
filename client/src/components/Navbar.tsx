a774d57ba6fe0280a2efb7d4770cd99793dfee69
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-30 22:22:30 +0900

    feat: add navbar app name and logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import Stirfry from '../styles/images/stirfry.gif'
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-20 shadow-xl z-[100]'
+    <div className='fixed w-full h-16 shadow-xl z-[100]'
     >
         <div className='bg-green-400 flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-            Navbar
+          <div className='flex h-full items-center'>
+            <div>
+              <img
+                alt='logo'
+                className='w-[45px] h-[45px]'
+                src={ Stirfry }></img>
+            </div>
+            <h1 className='text-3xl font-bold uppercase'>Pantry</h1>
+          </div>
         </div>
     </div>
   )
