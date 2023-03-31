792546adc76db8b13e1d6dd6796e46fb7211bd65
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-31 09:22:10 +0900

    feat: add isSelected column

---
+++ db/migrations/20230330232911_alter_pantry_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  return await knex.schema.alterTable(('pantry'), (table) => {
+    table.boolean('isSelected').notNullable();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  return await knex.schema.alterTable(('pantry'), (table) => {
+    table.dropColumn('isSelected');
+  })
+};
