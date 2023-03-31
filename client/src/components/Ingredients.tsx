b52263746bdaf0f0fce6b5b777c71224345df5d1
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-31 09:23:21 +0900

    feat: implement selected items to show up to ingredients component

--- client/src/components/Ingredients.tsx
+++ client/src/components/Ingredients.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
-const Ingredients = () => {
+interface IngredientsObj {
+  // id: number,
+  name: string,
+  // img_source: string,
+  // exp_date: string,
+  // fat: number,
+  // cal: number,
+  // protein: number,
+  // isSelected: boolean,
+  // qty: number,
+}
+
+interface IngredientsProps {
+  // setTrigger: (arg: boolean) => void;
+  trigger: boolean
+}
+
+const Ingredients: React.FC<IngredientsProps> = ({ trigger }) => {
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  useEffect(() => {
+    getSelectedItems();
+  },[trigger])
+
+  const getSelectedItems = async () => {
+    const fetchedSelected = await axios.get('/api/pantry/1goodsir/selected');
+    const selected = fetchedSelected.data.map((foodItem: IngredientsObj) => {
+      return foodItem.name;
+    })
+    setSelectedItems(selected);
+  }
+
   return (
     <div className='pt-6'>
         <div className='flex h-[250px] md:h-[355px] flex-col rounded-3xl shadow-lg bg-white bg-opacity-80 pt-5 border-2 border-white'>
-
+          {selectedItems}
         </div>
     </div>
   )
