e0e57ca82439d854d7b3fc20647086f26aaa7b83
Ed Cuevas <edjanncuevas@icloud.com>
2023-04-03 23:25:22 +0900

    feat: implement navigation when clicking logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -9,13 +9,15 @@ const Navbar = () => {
     <div className='fixed w-full h-16 shadow-lg z-[100]'
     >
         <div className='bg-green-400 flex justify-around items-center w-full h-full px-2 2xl:px-16'>
-          <div className='flex h-full items-center'>
+          <div
+            onClick={() => navigate('/')}
+            className='flex h-full items-center cursor-pointer'
+          >
             <div>
               <img
                 alt='logo'
                 className='w-[45px] h-[45px]'
                 src={ Stirfry }
-                onClick={() => navigate('/')}
               />
             </div>
             <h1 className='text-3xl font-bold uppercase'>Pan•try</h1>
