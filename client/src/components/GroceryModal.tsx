e3bd10df4f3bf57abceb559467356a8e5104d8fd
Ed Cuevas <edjanncuevas@icloud.com>
2023-04-05 00:35:16 +0900

    feat: create grocery list modal

---
+++ client/src/components/GroceryModal.tsx
@@ -0,0 +1,78 @@
+import { Transition } from '@headlessui/react'
+import { useEffect, useState } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+
+interface RecipeModalProps {
+    recipe: RecipeTableObj;
+    handleCloseModal: () => void;
+    handleLink: (arg: string) => void;
+}
+
+const RecipeModal: React.FC<RecipeModalProps> = ({ recipe, handleCloseModal, handleLink }) => {
+    const [recipeLines, setRecipeLines] = useState<JSX.Element[]>([]);
+
+    const handleClose = () => {
+        setTimeout(() => {
+            handleCloseModal();
+        }, 300);
+    }
+
+    useEffect(() => {
+        const ingredients = recipe.ingredientLines.map((line: string) => {
+            return (
+                <li>{line}</li>
+            );
+        });
+        setRecipeLines(ingredients);
+    },[]);
+
+
+  return (
+    <Transition
+      show={true}
+      enter="transition-opacity duration-300"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-300"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75"></div>
+          <div className="bg-white rounded-lg shadow-lg p-4 z-10 w-[380px]">
+            <div className="flex flex-col w-full min-h-full">
+                <div className='h-[400px] flex flex-col justify-between'>
+                    <div className='h-[130px] flex justify-start items-center'>
+                        <img
+                            className='h-[120px]'
+                            alt={recipe.label}
+                            src={recipe.image}
+                        />
+                        <div className='w-[220px] h-full flex justify-center items-center p-6'>
+                            <label className='font-bold uppercase'>{ recipe.label }</label>
+                        </div>
+                    </div>
+                    <div className='h-[200px] overflow-y-scroll'>
+                        <div className=''>
+                            {recipeLines}
+                        </div>
+                    </div>
+                    <div
+                        className='h-[40px] w-full rounded-xl text-white flex items-center justify-center bg-amber-600 hover:scale-105 ease-in duration-500 cursor-pointer'
+                        onClick={handleClose}
+                    >
+                        <p className='flex justify-center items-center w-20 h-[30px] uppercase tracking-widest'>
+                            <AiOutlineClose />
+                        </p>
+                   </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  )
+}
+
+export default RecipeModal;
